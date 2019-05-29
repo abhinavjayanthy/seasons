8e9846c2a3e4c633a736614b68b475e02070156a
abhinavjayanthy <abhinavjayanthy@icloud.com>
2019-05-29 11:38:37 -0400

    first of state check comments for the dos and do not's

--- src/index.js
+++ src/index.js
@@ -12,12 +12,23 @@ import ReactDOM from 'react-dom';
 
 //Below is a class based component 
 class App extends React.Component {
-    render () {
+    constructor(props){
+        super(props);
+        // THIS IS THE ONLY TIME WE DO THAT IS WHEN WE INITALIZE THE STATE
+        this.state = {lat:null};
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
+            (position) =>{
+                console.log(position)
+                this.setState({lat : position.coords.latitude})
+                // DO NOT DO :
+                // this.state.lat = position.coords.latitude
+            },
             (err) => console.log(err)
         )
-        return <div> Lattitude : </div>
+    }
+    //Have to define a render method 
+    render () {
+        return <div> Lattitude : {this.state.lat}</div>
     }
 }
 
