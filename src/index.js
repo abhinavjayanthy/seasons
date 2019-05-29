5d5ad26d96eb88455e8ec4bee236e4f8e6622bcc
abhinavjayanthy <abhinavjayanthy@icloud.com>
2019-05-29 11:16:29 -0400

    class based component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    )
-    // Cannot update the div here with the location as this is a function based componet so should move to class based componets 
-    return <div>Hi there!!</div>
-};
+class App extends React.Component {
+    render () {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        )
+        return <div> Lattitude : </div>
+    }
+}
 
 ReactDOM.render(
     <App />,
