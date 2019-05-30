f28682548394a4ebf2b276c20db7df391c718134
abhinavjayanthy <abhinavjayanthy@icloud.com>
2019-05-30 11:27:37 -0400

    summer winter implemntation

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,36 @@
+/* eslint-disable */
+import './SeasonDisplay.css'
 import React from 'react';
 
-const SeasonDisplay = () => {
-    return <div>Season Display</div>
+const seasonConfig = {
+    summer :{
+        iconName:'sun',
+        text:'Let\'s hit the beach'
+    },
+    winter :{
+        iconName:'snowflake',
+        text:'Burr, it is chilly'
+    }
+}
+
+
+const getSeason = (lat,month) =>{
+    if(month > 2 && month < 9 ){
+        return lat > 0  ? 'summer' : 'winter';
+    }else{
+        return lat > 0 ? 'winter' : 'summer'
+    }
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.withLat,new Date().getMonth())
+    return (
+        <div className={`season-display ${season}`}>
+            <i className = {`icon-left massive ${seasonConfig[season].iconName} icon`} />
+            <h1>{seasonConfig[season].text}</h1>
+            <i className = {`icon-right massive ${seasonConfig[season].iconName} icon`} />
+        </div>
+    );
 }
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+/* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay'
 
 // Below is function based component 
 // const App = () => {
@@ -12,23 +13,37 @@ import ReactDOM from 'react-dom';
 
 //Below is a class based component 
 class App extends React.Component {
+    // Do not do data loading in a contructor for more clear code 
     constructor(props){
         super(props);
-        // THIS IS THE ONLY TIME WE DO THAT IS WHEN WE INITALIZE THE STATE
-        this.state = {lat:null};
+        // Not best practise 
+        // this.state = {lat:null, errorMessage : ''};
+    }
+
+    //Same as writing in construtor babel will do the constructor initiliazing  
+    state = {
+        lat: null, 
+        errorMessage : ''
+    }
+    // Beast practise 
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            (position) =>{
-                console.log(position)
-                this.setState({lat : position.coords.latitude})
+            (position) =>this.setState({lat : position.coords.latitude}),
                 // DO NOT DO :
-                // this.state.lat = position.coords.latitude
-            },
-            (err) => console.log(err)
+                // this.state.lat = position.coords.latitude,
+            (err) => this.setState({errorMessage : err.message})
         )
     }
-    //Have to define a render method 
+    //Have to define a render method its only about returing jsx nothing else 
     render () {
-        return <div> Lattitude : { this.state.lat }</div>
+        if(this.state.errorMessage  && !this.state.lat){
+            return <div>Error : { this.state.errorMessage }</div>
+        }else if (!this.state.errorMessage  && this.state.lat){
+            // State is going as a prop here 
+            return <SeasonDisplay withLat = {this.state.lat}/>
+        }
+        return <i class="massive spinner loading icon"></i>
+        
     }
 }
 
